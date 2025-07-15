8c0341eeadf86fe5fcdf5fca3c926f3d46827de7
Oleksandr Bodnarchuk <oleksandr.bodnarchuk@atos.net>
2025-07-15 06:12:25 +0200

    groupdId & artifactId fix

--- src/test/java/pl/alex/app/users/UsersServiceApplicationTests.java
+++ src/test/java/pl/alex/app/users/UsersServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class UsersServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
