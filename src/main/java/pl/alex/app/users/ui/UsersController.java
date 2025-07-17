ab2fa6f1259b914b802cfd4f1fd5c623dc7437e9
Oleksandr Bodnarchuk <oleksandr.bodnarchuk@atos.net>
2025-07-17 07:25:18 +0200

    routing fix & multiple instances enabled

--- src/main/java/pl/alex/app/users/ui/UsersController.java
+++ src/main/java/pl/alex/app/users/ui/UsersController.java
@@ -1,11 +1,9 @@
 package pl.alex.app.users.ui;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 public class UsersController {
 
     @GetMapping("/status/check")
